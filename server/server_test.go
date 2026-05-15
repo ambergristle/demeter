67a225261ec961ac70320db600ab732981f02e07
ambergristle <101149854+ambergristle@users.noreply.github.com>
2026-05-15 14:00:20 -0700

    tests: add mux to extract path param

--- server/server_test.go
+++ server/server_test.go
@@ -4,32 +4,39 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"net/url"
+	"strconv"
 	"strings"
 	"testing"
+	"time"
 )
 
 func TestReadingRelay(t *testing.T) {
-	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	cbServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 	}))
-	defer server.Close()
+	defer cbServer.Close()
 
+	// #region Construct Request
 	body := strings.NewReader(url.Values{
-		"timestamp":    {"1778870328"},
+		"timestamp":    {strconv.FormatInt(time.Now().Unix(), 10)},
 		"humidity":     {"50.0"},
 		"temperature":  {"30.35"},
 		"air_pressure": {"0"},
 		"brightness":   {"10"},
 	}.Encode())
 
-	req := httptest.NewRequest("POST", "http://localhost:8080/readings/TEST_ID", body)
+	req := httptest.NewRequest("POST", "/readings/TEST_ID", body)
 	req.Header.Add("content-type", "application/x-www-form-urlencoded")
+	// #endregion
+
+	// Use Mux to extract path param
+	mux := http.NewServeMux()
+	mux.HandleFunc("/readings/{sensorId}", readingHandler(cbServer.URL, "TEST_SECRET"))
 
 	w := httptest.NewRecorder()
-	readingHandler(server.URL, "TEST_SECRET")(w, req)
+	mux.ServeHTTP(w, req)
 
 	resp := w.Result()
-
 	if resp.StatusCode != 200 {
 		t.Errorf("Callback failed with status: %d", resp.StatusCode)
 	}
