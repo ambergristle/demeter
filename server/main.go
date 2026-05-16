fd076ca91277645bb38bacdbfa9be3e07c4db392
ambergristle <101149854+ambergristle@users.noreply.github.com>
2026-05-15 17:24:23 -0700

    fix: pass flags through `main`

--- server/main.go
+++ server/main.go
@@ -4,17 +4,25 @@ import "flag"
 
 func main() {
 	var callbackUrl string
-	flag.StringVar(&callbackUrl, "cb", "", "The callback URL readings are relayed to")
-	flag.Parse()
-
-	// Validate configured callback URL
+	flag.StringVar(&callbackUrl, "cb", "", "The callback URL readings are relayed to.")
 	if len(callbackUrl) == 0 {
 		panic("No callback URL configured")
 	}
 
+	var port int
+	flag.IntVar(&port, "p", 8080, "The port to serve backend on, default: `8080`.")
+
+	var secret string
+	flag.StringVar(&secret, "p", "", "The secret used to sign callback requests.")
+	if len(secret) == 0 {
+		panic("No signing secret configured")
+	}
+
+	flag.Parse()
+
 	server(&DemeterOptions{
-		callbackUrl: "http://localhost:8000",
-		port:        8080,
-		secret:      "MOCK_SECRET",
+		callbackUrl: callbackUrl,
+		port:        port,
+		secret:      secret,
 	})
 }
