93aab4059920151be096d774ed36da00233b1dc3
ambergristle <101149854+ambergristle@users.noreply.github.com>
2026-05-15 17:23:32 -0700

    tests: validate payload is relayed to callback

--- server/server_test.go
+++ server/server_test.go
@@ -11,14 +11,32 @@ import (
 )
 
 func TestReadingRelay(t *testing.T) {
+	var payload ReadingPayload
+
 	cbServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, "Bad request: "+err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		values := r.Form
+		payload = ReadingPayload{
+			timestamp:    values.Get("timestamp"),
+			temperature:  values.Get("temperature"),
+			humidity:     values.Get("humidity"),
+			air_pressure: values.Get("air_pressure"),
+			brightness:   values.Get("brightness"),
+		}
+
 		w.WriteHeader(http.StatusOK)
 	}))
 	defer cbServer.Close()
 
 	// #region Construct Request
+	timestamp := strconv.FormatInt(time.Now().Unix(), 10)
 	body := strings.NewReader(url.Values{
-		"timestamp":    {strconv.FormatInt(time.Now().Unix(), 10)},
+		"timestamp":    {timestamp},
 		"humidity":     {"50.0"},
 		"temperature":  {"30.35"},
 		"air_pressure": {"0"},
@@ -39,5 +57,22 @@ func TestReadingRelay(t *testing.T) {
 	resp := w.Result()
 	if resp.StatusCode != 200 {
 		t.Errorf("Callback failed with status: %d", resp.StatusCode)
+	}
+
+	// todo: hardcoding bad
+	if payload.timestamp != timestamp {
+		t.Errorf("Unexpected humidity value")
+	}
+	if payload.humidity != "50.0" {
+		t.Errorf("Unexpected humidity value")
+	}
+	if payload.temperature != "30.35" {
+		t.Errorf("Unexpected temperature value")
+	}
+	if payload.air_pressure != "0" {
+		t.Errorf("Unexpected air_pressure value")
+	}
+	if payload.brightness != "10" {
+		t.Errorf("Unexpected brightness value")
 	}
 }
