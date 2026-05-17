19d697e0e24fdc3c58885e3eee9da191888faa9e
ambergristle <101149854+ambergristle@users.noreply.github.com>
2026-05-16 21:18:03 -0700

    fix: access flags after parse, convert secret to bytes at top level

--- server/main.go
+++ server/main.go
@@ -1,28 +1,33 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"log"
+)
 
 func main() {
 	var callbackUrl string
 	flag.StringVar(&callbackUrl, "cb", "", "The callback URL readings are relayed to.")
-	if len(callbackUrl) == 0 {
-		panic("No callback URL configured")
-	}
+
+	var secret string
+	flag.StringVar(&secret, "s", "", "The secret used to sign callback requests.")
 
 	var port int
 	flag.IntVar(&port, "p", 8080, "The port to serve backend on, default: `8080`.")
 
-	var secret string
-	flag.StringVar(&secret, "p", "", "The secret used to sign callback requests.")
-	if len(secret) == 0 {
-		panic("No signing secret configured")
+	flag.Parse()
+
+	if len(callbackUrl) == 0 {
+		log.Fatalf("No callback URL configured")
 	}
 
-	flag.Parse()
+	if len(secret) == 0 {
+		log.Fatalf("No signing secret configured")
+	}
 
 	server(&DemeterOptions{
 		callbackUrl: callbackUrl,
 		port:        port,
-		secret:      secret,
+		secret:      []byte(secret),
 	})
 }
