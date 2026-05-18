1f293118c93616a7494ff6331464b2d619d43611
ambergristle <101149854+ambergristle@users.noreply.github.com>
2026-05-18 10:04:12 -0700

    refactor: close over env init (setting secret flag)

--- server/main.go
+++ server/main.go
@@ -1,38 +1,26 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
 func main() {
-	var callbackUrl string
-	flag.StringVar(&callbackUrl, "cb", "", "The callback URL readings are relayed to.")
-
-	var secret string
-	flag.StringVar(&secret, "s", "", "The secret used to sign callback requests.")
-
-	var port int
-	flag.IntVar(&port, "p", 8080, "The port to serve backend on, default: `8080`.")
-
-	flag.Parse()
-
-	if len(callbackUrl) == 0 {
-		log.Fatalf("missing required flag: -cb")
-	}
-
-	if len(secret) == 0 {
-		log.Fatalf("missing required flag: -s")
+	env, err := initEnv()
+	if err != nil {
+		log.Fatalln(err)
 	}
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/readings/{sensorId}", readingHandler(callbackUrl, []byte(secret)))
+	mux.HandleFunc("/readings/{sensorId}", readingHandler(env.callbackUrl))
 
 	srv := &http.Server{
-		Addr:         fmt.Sprintf(":%d", port),
+		Addr:         fmt.Sprintf(":%d", env.port),
 		ReadTimeout:  2 * time.Second,
 		WriteTimeout: 2 * time.Second,
 		// IdleTimeout:  120 * time.Second,
@@ -40,5 +28,35 @@ func main() {
 	}
 
 	log.Printf("Server listening on %s\n", srv.Addr)
-	log.Fatal(srv.ListenAndServe())
+	log.Fatalln(srv.ListenAndServe())
+}
+
+func initEnv() (SafeEnv, error) {
+	var callbackUrl string
+	flag.StringVar(&callbackUrl, "cb", "", "The callback URL readings are relayed to.")
+
+	var port int
+	flag.IntVar(&port, "p", 8080, "The port to serve backend on, default: `8080`.")
+
+	var secret string
+	flag.StringVar(&secret, "s", "", "The secret used to sign callback requests.")
+
+	flag.Parse()
+
+	if callbackUrl == "" {
+		return SafeEnv{}, errors.New("Missing required flag: -cb")
+	}
+
+	if secret == "" {
+		return SafeEnv{}, errors.New("Missing required flag: -s")
+	}
+
+	os.Setenv("SIGNING_SECRET", secret)
+
+	return SafeEnv{callbackUrl: callbackUrl, port: port}, nil
+}
+
+type SafeEnv struct {
+	callbackUrl string
+	port        int
 }
