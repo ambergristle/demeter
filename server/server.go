1ee0bbd9dec887519e7d1e42fbe4e05f907f5924
ambergristle <101149854+ambergristle@users.noreply.github.com>
2026-05-18 10:04:55 -0700

    refactor: get secret from env, move timestamp creation into signature generation

--- server/server.go
+++ server/server.go
@@ -9,12 +9,13 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"os"
 	"strconv"
 	"strings"
 	"time"
 )
 
-func readingHandler(callbackUrl string, secret []byte) func(w http.ResponseWriter, r *http.Request) {
+func readingHandler(callbackUrl string) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		case "POST":
@@ -38,7 +39,7 @@ func readingHandler(callbackUrl string, secret []byte) func(w http.ResponseWrite
 			}
 
 			go func(r ReadingPayload) {
-				if err := dispatchEvent(callbackUrl, r, secret); err != nil {
+				if err := postCallback(callbackUrl, r); err != nil {
 					log.Printf("Callback failed repeatedly: %v ", err)
 				}
 			}(reading)
@@ -53,7 +54,7 @@ func readingHandler(callbackUrl string, secret []byte) func(w http.ResponseWrite
 
 func parseReading(values *url.Values) (ReadingPayload, error) {
 	ts := values.Get("timestamp")
-	if !validateTimestamp(ts) {
+	if !isValidTS(ts) {
 		return ReadingPayload{}, errors.New("Invalid parameter: timestamp")
 	}
 
@@ -102,8 +103,8 @@ func isIntStr(s string) bool {
 	return err == nil
 }
 
-func validateTimestamp(timestamp string) bool {
-	tsInt, err := strconv.ParseInt(timestamp, 10, 64)
+func isValidTS(ts string) bool {
+	tsInt, err := strconv.ParseInt(ts, 10, 64)
 	if err != nil {
 		return false
 	}
@@ -118,9 +119,7 @@ func abs(x int64) int64 {
 	return x
 }
 
-func dispatchEvent(callbackUrl string, payload ReadingPayload, secret []byte) error {
-	sigTS := strconv.FormatInt(time.Now().Unix(), 10)
-
+func postCallback(cbUrl string, payload ReadingPayload) error {
 	// Initialize bodyVals first to generate signature
 	bodyVals := url.Values{
 		"timestamp":    {payload.timestamp},
@@ -131,7 +130,7 @@ func dispatchEvent(callbackUrl string, payload ReadingPayload, secret []byte) er
 	}
 	bodyStr := bodyVals.Encode()
 
-	sig := formatSignature(bodyStr, sigTS, secret)
+	sig := formatSignature(bodyStr)
 
 	// #region Construct Request
 	// Should this be happening for each request?
@@ -142,13 +141,13 @@ func dispatchEvent(callbackUrl string, payload ReadingPayload, secret []byte) er
 	}
 
 	var (
-		req *http.Request
-		res *http.Response
-		err error
+		req  *http.Request
+		resp *http.Response
+		err  error
 	)
 	tries := 0
 	for tries < 3 {
-		req, err = http.NewRequest("POST", callbackUrl, strings.NewReader(bodyStr))
+		req, err = http.NewRequest("POST", cbUrl, strings.NewReader(bodyStr))
 		if err != nil {
 			// Client or configuration error,
 			// Exit early
@@ -156,20 +155,20 @@ func dispatchEvent(callbackUrl string, payload ReadingPayload, secret []byte) er
 		}
 
 		req.Header.Add("content-type", "application/x-www-form-urlencoded")
-		req.Header.Add("x-dmtr-timestamp", sigTS)
-		req.Header.Add("x-dmtr-signature", sig)
+		req.Header.Add("x-dmtr-timestamp", sig.timestamp)
+		req.Header.Add("x-dmtr-signature", sig.signature)
 		// #endregion
 
-		res, err = client.Do(req)
+		resp, err = client.Do(req)
 		if err != nil {
 			// Client or configuration error,
 			// Exit early
 			break
 		}
-		defer res.Body.Close()
+		defer resp.Body.Close()
 		// Fail if body has length?
 
-		if res.StatusCode < 500 {
+		if resp.StatusCode < 500 {
 			// Success, or non-recoverable error,
 			// Exit early
 			break
@@ -184,26 +183,11 @@ func dispatchEvent(callbackUrl string, payload ReadingPayload, secret []byte) er
 		return err
 	}
 
-	if res.StatusCode == http.StatusOK {
+	if resp.StatusCode == http.StatusOK {
 		return nil
 	}
 
-	return errors.New("Callback failed with status: " + res.Status)
-}
-
-func formatSignature(body string, ts string, secret []byte) string {
-	signatureBase := fmt.Sprintf("v0:%s:%s", ts, body)
-
-	mac := hmac.New(sha256.New, secret)
-	mac.Write([]byte(signatureBase))
-
-	return fmt.Sprintf("v0=%s", hex.EncodeToString(mac.Sum(nil)))
-}
-
-type DemeterOptions struct {
-	callbackUrl string
-	port        int
-	secret      []byte
+	return errors.New("Callback failed with status: " + resp.Status)
 }
 
 type ReadingPayload struct {
@@ -212,4 +196,28 @@ type ReadingPayload struct {
 	temperature  string
 	air_pressure string
 	brightness   string
+}
+
+func formatSignature(body string) HmacSignature {
+	ts := strconv.FormatInt(time.Now().Unix(), 10)
+	sigBase := fmt.Sprintf("v0:%s:%s", ts, body)
+
+	secret := []byte(os.Getenv("SIGNING_SECRET"))
+	// This only clears the bytes;
+	// The string allocated for `Getenv` is inaccessible
+	defer func() { clear(secret) }()
+
+	mac := hmac.New(sha256.New, secret)
+
+	mac.Write([]byte(sigBase))
+
+	return HmacSignature{
+		signature: fmt.Sprintf("v0=%s", hex.EncodeToString(mac.Sum(nil))),
+		timestamp: ts,
+	}
+}
+
+type HmacSignature struct {
+	signature string
+	timestamp string
 }
